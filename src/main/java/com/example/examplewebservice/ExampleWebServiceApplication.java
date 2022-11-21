c0e46523e8597a408f6734721cffc0028a71ed2f
Odegaa <odegaa0202@gmail.com>
2022-11-21 22:13:53 +0500

    create project

---
+++ src/main/java/com/example/examplewebservice/ExampleWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examplewebservice/ExampleWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examplewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
